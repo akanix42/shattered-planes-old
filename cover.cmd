455a9681bf062a532476f5c5347b07fd3cbed9e8
Nathan Reid <nathan-code@nathantreid.com>
2016-05-16 22:28:09 -0500

    add code coverage

---
+++ cover.cmd
@@ -0,0 +1,1 @@
+node_modules\.bin\istanbul C:\Users\nathantreid\AppData\Roaming\npm\node_modules\mocha\bin\_mocha -- --compilers js:babel-register --require source-map-support/register imports/**/*.tests.js
