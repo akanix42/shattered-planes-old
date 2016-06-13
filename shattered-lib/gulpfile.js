de8e45ef4c7c41f648e66d74b9f7dbfd5c1bc364
Nathan Reid <nathan-code@nathantreid.com>
2016-06-13 18:05:03 -0500

    prevent gulp from crashing on errors

--- shattered-lib/gulpfile.js
+++ shattered-lib/gulpfile.js
@@ -2,10 +2,12 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const watch = require('gulp-watch');
+const plumber = require('gulp-plumber');
 
 gulp.task('transpile', () =>
   gulp.src('src/**/*.js')
     .pipe(watch('src/**/*.js'))
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write('.'))
