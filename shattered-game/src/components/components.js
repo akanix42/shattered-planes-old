60ebfe8ed7f8efb44219684b78498569aea6b685
Nathan Reid <nathan-code@nathantreid.com>
2016-06-13 18:18:12 -0500

    update autogenerated components file

--- shattered-game/src/components/components.js
+++ shattered-game/src/components/components.js
@@ -2,16 +2,16 @@
 import CollideComponent from './CollideComponent.js';
 import CollideComponent_tests from './CollideComponent.tests.js';
 import NormalMovementComponent from './NormalMovementComponent.js';
-import NormalMovementComponent_tests from './NormalMovementComponent.tests.js';
 import PositionComponent from './PositionComponent.js';
+import NormalMovementComponent_tests from './NormalMovementComponent.tests.js';
 import PositionComponent_tests from './PositionComponent.tests.js';
 
 const importRegistrations = {};
 importRegistrations[CollideComponent.name] = CollideComponent;
 importRegistrations[CollideComponent_tests.name] = CollideComponent_tests;
 importRegistrations[NormalMovementComponent.name] = NormalMovementComponent;
-importRegistrations[NormalMovementComponent_tests.name] = NormalMovementComponent_tests;
 importRegistrations[PositionComponent.name] = PositionComponent;
+importRegistrations[NormalMovementComponent_tests.name] = NormalMovementComponent_tests;
 importRegistrations[PositionComponent_tests.name] = PositionComponent_tests;
 
 export default importRegistrations;
