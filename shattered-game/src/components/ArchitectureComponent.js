f90396d997794426bb06521b805c54a8b4eb0121
Nathan Reid <nathan-code@nathantreid.com>
2016-06-14 19:26:36 -0500

    add architecture component (for floors, walls, etc)

---
+++ shattered-game/src/components/ArchitectureComponent.js
@@ -0,0 +1,28 @@
+'use strict';
+import {serializable} from 'jsonc';
+import Component from 'shattered-lib/Component';
+import events from '/events';
+
+@serializable('ArchitectureComponent')
+class ArchitectureComponent extends Component {
+  constructor() {
+    super();
+    this.addHandler(events.position, 150, this.onPosition);
+  }
+
+  onPosition(event) {
+    if (event.destination === this.entity.tile)
+      return;
+
+    const previousTile = this.entity.tile;
+    if (previousTile && previousTile.architecture === this.entity)
+      previousTile.architecture = null;
+
+    if (event.destination)
+      event.destination.architecture = this.entity;
+  }
+}
+
+ArchitectureComponent._name = 'architecture';
+
+export default ArchitectureComponent;
---
+++ shattered-game/src/components/ArchitectureComponent.tests.js
@@ -0,0 +1,35 @@
+'use strict';
+import ArchitectureComponent from './ArchitectureComponent';
+import events from '/events';
+import Entity from 'shattered-lib/Entity';
+import Tile from 'shattered-lib/Tile';
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('ArchitectureComponent', ()=> {
+  describe('onPosition', () => {
+
+    it(`should set the current tile's architecture to it's entity`, () => {
+      const architectureComponent = new ArchitectureComponent();
+      const entity = new Entity();
+      const currentTile = new Tile();
+      entity.addComponent(architectureComponent);
+
+      architectureComponent.onPosition({destination: currentTile});
+      expect(currentTile.architecture).to.equal(entity);
+    });
+
+    it(`should remove it's entity from the previous tile's architecture`, () => {
+      const architectureComponent = new ArchitectureComponent();
+      const entity = new Entity();
+      const previousTile = new Tile();
+      entity.addComponent(architectureComponent);
+      previousTile.architecture = entity;
+
+      architectureComponent.onPosition({destination: null});
+      expect(previousTile.architecture).to.be.null;
+    });
+
+  });
+});
