8d27322c322fb019dcb71211c9ae91201210d1e7
Nathan Reid <nathan-code@nathantreid.com>
2016-06-15 10:24:29 -0500

    remove transpiled files from lib

--- shattered-lib/Map.js
+++
@@ -1,8 +0,0 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self, call) {if (!self) {throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call && (typeof call === "object" || typeof call === "function") ? call : self;}function _inherits(subClass, superClass) {if (typeof superClass !== "function" && superClass !== null) {throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);}subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;}var 
-
-Map = function (_Array) {_inherits(Map, _Array);function Map() {var _Object$getPrototypeO;var _temp, _this, _ret;_classCallCheck(this, Map);for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {args[_key] = arguments[_key];}return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(Map)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.
-    level = null, _temp), _possibleConstructorReturn(_this, _ret);}return Map;}(Array);exports.default = 
-
-
-Map;
-//# sourceMappingURL=Map.js.map
--- shattered-lib/events.js
+++
@@ -1,6 +0,0 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });
-
-var events = {};exports.default = 
-
-events;
-//# sourceMappingURL=events.js.map
