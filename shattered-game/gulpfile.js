d1df099798dfa4225d1dc34d9834d4015e42afec
Nathan Reid <nathan-code@nathantreid.com>
2016-06-15 11:06:04 -0500

    disable gulp files

--- shattered-game/gulpfile.js
+++ shattered-game/gulpfile.js
@@ -3,15 +3,15 @@ const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const watch = require('gulp-watch');
 const plumber = require('gulp-plumber');
-
-gulp.task('default', ['transpile']);
-
-gulp.task('transpile', () =>
-  gulp.src('src/**/*.js')
-    .pipe(watch('src/**/*.js'))
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('.'))
-);
+//
+// gulp.task('default', ['transpile']);
+//
+// gulp.task('transpile', () =>
+//   gulp.src('src/**/*.js')
+//     .pipe(watch('src/**/*.js'))
+//     .pipe(plumber())
+//     .pipe(sourcemaps.init())
+//     .pipe(babel())
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest('.'))
+// );
