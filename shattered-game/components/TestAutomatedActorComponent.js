9c5027d05265529827667d75f610633e0878116f
Nathan Reid <nathan-code@nathantreid.com>
2016-07-10 00:22:27 -0500

    optimizing - only return promise if there is a timeout

--- shattered-game/components/TestAutomatedActorComponent.js
+++ shattered-game/components/TestAutomatedActorComponent.js
@@ -1,5 +1,5 @@
 'use strict';
-import {serializable} from 'shattered-lib/lib/jsonc';
+import { serializable } from 'shattered-lib/lib/jsonc';
 import ActorComponent from './ActorComponent';
 import global from '/global';
 import ROT from 'shattered-lib/lib/rot-js';
@@ -13,22 +13,23 @@ class TestAutomatedActorComponent extends ActorComponent {
   }
 
   act() {
-    return new Promise((resolve)=> {
-      const performAction = () => {
-        const tile = this.entity.tile;
+    const performAction = () => {
+      const tile = this.entity.tile;
 
-        const x = ROT.RNG.getUniformInt(Math.max(0, tile.point.x - 1), Math.min(tile.level._map.width - 1, tile.point.x + 1));
-        const y = ROT.RNG.getUniformInt(Math.max(0, tile.point.y - 1), Math.min(tile.level._map.height - 1, tile.point.y + 1));
-        var event = {name: events.move, destination: tile.level.getTileAtXY(x, y)};
-        this.entity.emit(event);
+      const x = ROT.RNG.getUniformInt(Math.max(0, tile.point.x - 1), Math.min(tile.level._map.width - 1, tile.point.x + 1));
+      const y = ROT.RNG.getUniformInt(Math.max(0, tile.point.y - 1), Math.min(tile.level._map.height - 1, tile.point.y + 1));
+      var event = { name: events.move, destination: tile.level.getTileAtXY(x, y) };
+      this.entity.emit(event);
 
-        resolve(event.actionTime||this.entity.attributes.moveSpeed.current);
-      };
-      if (this._timeout)
-        setTimeout(performAction, this._timeout);
-      else
-        performAction();
-    });
+      return event.actionTime || this.entity.attributes.moveSpeed.current;
+      // resolve(event.actionTime||this.entity.attributes.moveSpeed.current);
+    };
+    if (this._timeout)
+      return new Promise((resolve)=> {
+        setTimeout(()=>resolve(performAction), this._timeout);
+      });
+    else
+      return performAction();
   }
 }
 TestAutomatedActorComponent._name = 'testAutomatedActor';
