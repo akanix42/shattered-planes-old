4d96bbf58dd985571e9821f852d335481af576f2
Nathan Reid <nathan-code@nathantreid.com>
2016-07-16 21:49:25 -0500

    remove obsolete code left over from an incomplete commit

--- app-webpack/app/game/screens/inGameScreen.js
+++ app-webpack/app/game/screens/inGameScreen.js
@@ -46,7 +46,6 @@ class InGameScreen extends Screen {
   }
 
   renderTile(tile) {
-    const entityToRender = tile.occupants.slice(-1)[0] || tile.architecture;
     const entityToRender = tile.occupant || tile.architecture;
     this._display.draw(tile.point.x, tile.point.y, entityToRender.template.character, entityToRender.template.color, 'black');
   }
