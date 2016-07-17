5d471711fe75a2ffa0560895ae37d4e5b67d82d9
Nathan Reid <nathan-code@nathantreid.com>
2016-07-16 21:50:12 -0500

    serialize and deserialize Handlers

--- shattered-lib/event-system/Handler.js
+++ shattered-lib/event-system/Handler.js
@@ -1,17 +1,29 @@
 import idGenerator from '/generators/idGenerator';
-import { serializable } from '/lib/jsonc';
+import { serializable, Serializer, Deserializer } from '/lib/jsonc';
+import eventTypes from '/event-system/eventTypes';
 
 @serializable('Handler')
 export default class Handler {
   constructor(eventType, priority, context, callback) {
-    if (arguments.length === 0)
-      return;
-
     this.eventType = eventType;
     this.priority = priority;
-    this.eventType.addPriority(priority);
     this.context = context;
     this.callback = callback;
     this.id = idGenerator.generate(Handler.__type__);
+
+    if (arguments.length === 0) return;
+    /* Don't call during deserialization */
+    this.eventType.addPriority(priority);
+  }
+
+  [Serializer.Symbols.Serialize]() {
+    const obj = { ...this };
+    obj.eventType = obj.eventType.id;
+    return obj;
+  }
+
+  [Deserializer.Symbols.PostProcess]() {
+    this.eventType = eventTypes[eventTypes.eventLookup[this.eventType]];
+    this.eventType.addPriority(this.priority);
   }
 }
