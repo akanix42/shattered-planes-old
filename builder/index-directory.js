e0c5e9ddd95183c7c898c61add9d0b1ec4960243
Nathan Reid <nathan-code@nathantreid.com>
2016-07-18 22:15:22 -0500

    support indexing subdirectories

--- builder/index-directory.js
+++ builder/index-directory.js
@@ -9,21 +9,29 @@ module.exports = function indexDirectory(directory, globExcludePatterns, outputF
   recursive(directory, globExcludePatterns, function (err, files) {
     // Files is an array of filename
     if (!files) return;
+    fs.writeFileSync(outputFile, '');
     // console.log(files);
     files = files.map(file=> file.replace(/\\/g, '/'));
     const importStatements = [];
     const importRegistrations = [];
 
     files.forEach(file=> {
-      let relativeFile = path.relative(directory, file);
-      relativeFile = path.basename(relativeFile, path.extname(relativeFile))
-      const importAs = relativeFile.replace(/\//g, '_').replace(/\./g, '_');
-      const importStatement = `import ${importAs} from './${relativeFile}';`;
-      const loadedFile = require(path.join(process.cwd(), file));
-      const registration = loadedFile.default._name || loadedFile.default.__type__;
-      const importRegistration = `importRegistrations.${registration} = ${useNew ? `new ${importAs}();` : importAs};`;
-      importStatements.push(importStatement);
-      importRegistrations.push(importRegistration);
+      try {
+        let relativeFile = path.relative(directory, file);
+        // console.log(relativeFile);
+        relativeFile = (`${path.dirname(relativeFile)}/${path.basename(relativeFile, path.extname(relativeFile))}`)
+          .replace(/\\/g, '/');
+        const importAs = relativeFile.replace(/\//g, '_').replace(/\./g, '_').replace(/-/g, '_');
+        const importStatement = `import ${importAs} from './${relativeFile}';`;
+        const loadedFile = require(path.join(process.cwd(), file));
+        const registration = loadedFile.default._name || loadedFile.default.__type__;
+        const importRegistration = `importRegistrations.${registration} = ${useNew ? `new ${importAs}();` : importAs};`;
+        importStatements.push(importStatement);
+        importRegistrations.push(importRegistration);
+      } catch (ex) {
+        console.log(file);
+        throw ex;
+      }
     });
 
     fs.writeFileSync(outputFile, getOutput(importStatements, importRegistrations));
